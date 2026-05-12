99c8f1808f7a679256e7f780bbc3d3b20f24e53f
Nutkamon Manosatcharak <ninenutkm@gmail.com>
2026-05-12 10:31:58 +0700

    fix: total cache annihilation - self-destruct sw and unregister script

--- sw.js
+++ sw.js
@@ -1,39 +1,12 @@
-const CACHE_NAME = 'hcu-landmarklens-v7';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', (event) => {
+/* Self-destructing Service Worker to clear stale cache */
+self.addEventListener('install', (e) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  // Use Network First for index.html and root to ensure latest version
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(event.request))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (e) => {
+  self.registration.unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach(client => client.navigate(client.url));
+    });
 });
